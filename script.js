fbd02dc0c3b4355fc62444bb61cb979e71f1f993
Hrishi <hrishiyadav54@gmail.com>
2025-08-27 00:18:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navLinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typed-text", {
+    strings: [
+      "Hi, I'm Hrishi",
+      "I am a Developer.",
+      "I am a Designer.",
+      "I am a Creator.",
+    ],
+    typeSpeed: 70, // typing speed
+    backSpeed: 40, // backspacing speed
+    backDelay: 1500, // pause before backspacing
+    loop: true, // keep repeating
+  });
+});
